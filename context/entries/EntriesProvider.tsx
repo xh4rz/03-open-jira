b055dd6e3b9fa54398c5c54d68c72368d61b1352
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-06-12 01:56:38 -0500

    107. Interface Entry

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,13 +1,34 @@
 import { FC, useReducer } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { Entry } from '../../interfaces';
 import { EntriesContext, entriesReducer } from './';
 
 export interface EntriesState {
-	entries: [];
+	entries: Entry[];
 	children?: React.ReactNode;
 }
 
 const Entries_INITIAL_STATE: EntriesState = {
-	entries: []
+	entries: [
+		{
+			_id: uuidv4(),
+			description: 'Descripción 1',
+			status: 'pending',
+			createdAt: Date.now()
+		},
+		{
+			_id: uuidv4(),
+			description: 'Descripción 2',
+			status: 'in-progress',
+			createdAt: Date.now() - 1000000
+		},
+		{
+			_id: uuidv4(),
+			description: 'Descripción 3',
+			status: 'finished',
+			createdAt: Date.now() - 100000
+		}
+	]
 };
 
 export const EntriesProvider: FC<EntriesState> = ({ children }) => {

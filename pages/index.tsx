a1377bc5408a9be11cb0beb908a9e7d4968b18d0
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-06-12 01:19:28 -0500

    105. Diseño de la pantalla principal - index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,32 @@
 import type { NextPage } from 'next';
-import { Typography } from '@mui/material';
+import { Card, CardContent, CardHeader, Grid } from '@mui/material';
 import { Layout } from '../components/layouts';
 
 const HomePage: NextPage = () => {
 	return (
-		<Layout>
-			<Typography variant="h1" color="primary">
-				Hola Mundo
-			</Typography>
+		<Layout title="Home - OpenJira">
+			<Grid container spacing={2}>
+				<Grid item xs={12} sm={4}>
+					<Card sx={{ height: 'calc(100vh - 100px)' }}>
+						<CardHeader title="Pendientes" />
+
+						<CardContent>
+							{/* Agregar una nueva entrada */}
+							{/* Listado de las entradas */}
+						</CardContent>
+					</Card>
+				</Grid>
+				<Grid item xs={12} sm={4}>
+					<Card sx={{ height: 'calc(100vh - 100px)' }}>
+						<CardHeader title="En Progreso" />
+					</Card>
+				</Grid>
+				<Grid item xs={12} sm={4}>
+					<Card sx={{ height: 'calc(100vh - 100px)' }}>
+						<CardHeader title="Completadas" />
+					</Card>
+				</Grid>
+			</Grid>
 		</Layout>
 	);
 };

2cc7c8d83e89b31e418fd747cc3b15e33c2e0b36
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-08-06 01:01:04 -0500

    134. Terminar nuestro endpoint de actualización

--- pages/api/entries/[id].ts
+++ pages/api/entries/[id].ts
@@ -47,14 +47,19 @@ const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 		status = entryToUpdate.status
 	} = req.body;
 
-	const updatedEntry = await Entry.findByIdAndUpdate(
-		id,
-		{
-			description,
-			status
-		},
-		{ runValidators: true, new: true }
-	);
-
-	res.status(200).json(updatedEntry!);
+	try {
+		const updatedEntry = await Entry.findByIdAndUpdate(
+			id,
+			{
+				description,
+				status
+			},
+			{ runValidators: true, new: true }
+		);
+		await db.disconnect();
+		res.status(200).json(updatedEntry!);
+	} catch (error: any) {
+		await db.disconnect();
+		res.status(400).json({ message: error.errors.status.message });
+	}
 };
